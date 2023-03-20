4f2afea0ba5a44aca35717c1ca4932f5c3a68427
TheoLisin <theo.lisin@gmail.com>
2023-03-20 17:17:10 +0100

    Change paths to abs paths

--- start_uvicorn.sh
+++ start_uvicorn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /sly-app-data/easy-diffusion/scripts
-cd ..
-export SD_UI_PATH=`pwd`/ui
-cd stable-diffusion
+set -e
+
+export SD_UI_PATH=/sly-app-data/easy-diffusion/ui
+cd /sly-app-data/easy-diffusion/stable-diffusion
 
 uvicorn main:server_api --app-dir "$SD_UI_PATH" --port ${SD_UI_BIND_PORT:-9000} --host ${SD_UI_BIND_IP:-0.0.0.0} --log-level error
